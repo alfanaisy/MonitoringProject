27d16a6d130212aab0c7d8dfedb1aa5a81b8ac84
alfanaisy <alfanaisy7@gmail.com>
2021-05-25 20:55:19 +0700

    Add project files.

---
+++ MonitoringProject - API/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringProject___API.Models
+{
+    [Table("TB_M_Role")]
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
---
+++ MonitoringProject - API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringProject___API.Models
+{
+    [Table("TB_M_User")]
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int RoleID { get; set; }
+        public Role Role { get; set; }
+    }
+}
