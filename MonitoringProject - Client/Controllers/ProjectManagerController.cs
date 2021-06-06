ddd2255a34b552bc20f77de17f354079ac97f6ef
Alfan Rihadatul Aisy <alfanaisy7@gmail.com>
2021-06-07 03:32:20 +0700

    small fix (fix convert DateTime to getTime (JS))

--- MonitoringProject - Client/Controllers/ProjectManagerController.cs
+++ MonitoringProject - Client/Controllers/ProjectManagerController.cs
@@ -490,8 +490,8 @@ namespace MonitoringProject___Client.Controllers
         public double MilliTimeStamp(DateTime TheDate)
         {
             DateTime d1 = new DateTime(1970, 1, 1);
-            DateTime d2 = TheDate.ToUniversalTime();
-            TimeSpan ts = new TimeSpan(d2.Ticks - d1.Ticks);
+            //DateTime d2 = TheDate.ToUniversalTime();
+            TimeSpan ts = new TimeSpan(TheDate.Ticks - d1.Ticks);
 
             return ts.TotalMilliseconds;
         }
